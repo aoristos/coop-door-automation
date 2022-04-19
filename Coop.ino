9f9dd9a44c065717401f9bada112294e997e0366
aoristos <jefbiesbrouck1@gmail.com>
2022-04-19 20:40:09 +0200

    add the libraries for the DS1307RTC Real-Time-Clock

--- Coop.ino
+++ Coop.ino
@@ -1,16 +1,39 @@
 /*
- * Code for MotorShield-L298N
- * "D:\Documents\Mijn PaperPort-documenten\Elektronica\Arduino\Shields\MotorShield-L298N-handleiding.pdf"
- * Sample code using functions motorUp() and motorDown()
+ * Open and close a chicken cooop door, using an Arduino, a MotorShield-L298N and a DS1307RTC
+ * 
+ * documentation:
+ *  "D:\Documents\Mijn PaperPort-documenten\Elektronica\Arduino\Shields\MotorShield-L298N-handleiding.pdf"
+ *  Using a Real Time Clock with Arduino ( https://dronebotworkshop.com/real-time-clock-arduino/ )
+ *  Controlling DC Motors with the L298N H Bridge and Arduino ( https://dronebotworkshop.com/dc-motors-l298n-h-bridge/ )
+ *  
+ * Hardware:
+ *  Arduino Uno
+ *  MotorShield-L298N
+ *  DS1307 Tiny RTC
+ *  
+ * include libraries:
+ *  <wire.h> the built-in library to communicate with the I2C bus
+ *  <TimeLib.h> library ( https://github.com/PaulStoffregen/Time/releases )
+ *  <DS1307RTC.h> library ( https://github.com/PaulStoffregen/DS1307RTC/releases )
+ * 
  */
+ 
+// * built-in library <wire.h> serves the communication with the I2C bus
+#include <Wire.h>
+
+// * https://github.com/PaulStoffregen/Time/releases
+#include <TimeLib.h>
+
+// * https://github.com/PaulStoffregen/DS1307RTC/releases
+#include <DS1307RTC.h>
 
 // motor 1
-byte EN1 = 6; // PWM
-byte IN1 = 7; // MOTOR1 UP
-byte IN2 = 8; // MOTOR1 DOWN
+const byte EN1 = 6; // PWM
+const byte IN1 = 7; // MOTOR1 UP
+const byte IN2 = 8; // MOTOR1 DOWN
+
+// Security
 int Runtime = 3000; // Security Runtime Limit
-
-
 
 void setup()
 {
