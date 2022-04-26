94b0792718cca845206c73ab5e8de45efcfdbf6f
aoristos <jefbiesbrouck1@gmail.com>
2022-04-26 20:29:06 +0200

    TEST: add Serial.print-statements to reveal the program logic

--- Coop.ino
+++ Coop.ino
@@ -1,4 +1,4 @@
-/*
+ /*
  * Open and close a chicken cooop door, using an Arduino Uno, a MotorShield-L298N and a DS1307RTC
  * 
  * documentation:
@@ -60,20 +60,20 @@
   const byte upperSwitch = 11; // SWITCH_IS_ACTIVATED when the door is fully open
   const byte lowerSwitch = 12; // SWITCH_IS_ACTIVATED when the door is fully closed
 
-  // Values based on NOAA_Solar_Calculations_year ( https://gml.noaa.gov/grad/solcalc/calcdetails.html )
-  // Calculations for Belgium-Bellegem latitude 50,50° / longitude 3,16°  Timezone GMT +1
-  // Times are GMT+1 ; no summertime (daylight saving time) 
-  // Values for day 8 of every month (day 8 is roughly medium between the season pivots at day 21)  
+// Values based on NOAA_Solar_Calculations_year ( https://gml.noaa.gov/grad/solcalc/calcdetails.html )
+// Calculations for Belgium-Bellegem latitude 50,50° / longitude 3,16°  Timezone GMT +1
+// Times are GMT+1 ; no summertime (daylight saving time) 
+// Values for day 8 of every month (day 8 is roughly medium between the season pivots at day 21)
 
   int sun_rise[12]={
- //Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
-   526, 491, 436, 368, 310, 277, 286, 325, 372, 419, 470, 515};
- // 08:46, 08:11, 07:16, 06:08, 05:09, 04:36, 04:45, 05:25, 06:12, 06:58, 07:50, 08:34     - Times are GMT+1 no summer saving as clock isn't adjusted
+  //Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
+  526, 491, 436, 368, 310, 277, 286, 325, 372, 419, 470, 515};
+  // 08:46, 08:11, 07:16, 06:08, 05:09, 04:36, 04:45, 05:25, 06:12, 06:58, 07:50, 08:34     - Times are GMT+1 no summer saving as clock isn't adjusted
 
   int sun_set[12] ={
- //Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
+  //Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
   1022, 1072, 1120, 1170, 1218, 1256, 1259, 1221, 1158, 1091, 1032, 1004};
- // 17:01, 17:51, 18:40, 19:30, 20:17, 20:56, 20:59, 20:20, 19:17, 18:11, 17:11, 16:43      - Times are GMT+1 no summer saving as clock isn't adjusted
+  // 17:01, 17:51, 18:40, 19:30, 20:17, 20:56, 20:59, 20:20, 19:17, 18:11, 17:11, 16:43      - Times are GMT+1 no summer saving as clock isn't adjusted
 
 
 // store the sunrise and sunset 'minutes after midnight'
@@ -83,7 +83,7 @@
   int clockTimeNow = 0;
 
 // flag to indicate daytime or nighttime
-  // 'nighTime = false' when it is day ; 'nighTime = true' when it is night
+// 'nighTime = false' when it is day ; 'nighTime = true' when it is night
   bool nightTime = false;
 
 // Security
@@ -91,10 +91,17 @@
   bool Alarm = false;
 // limit the runtime to protect damage when a switch is never activated
 // runtimeLimit depends on the motorspeed, the diameter of the spool and the door eleavtion height.
-  int RuntimeLimit = 5000; // Security runtime limit
+  int runTimeLimit = 5000; // Security runtime limit
 
 void setup()
 {
+
+ 
+// test print
+  Serial.println("Dit is void setup()");
+  delay(3000);
+
+  
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   
@@ -109,7 +116,13 @@ void setup()
 
 void loop()
 { 
-    tmElements_t tm;
+
+
+// test print
+  Serial.println("Dit is void loop()");
+  delay(3000);
+
+  tmElements_t tm;
 
   if (RTC.read(tm)) {
  
@@ -131,8 +144,8 @@ void loop()
 	
     nightTime = checkNightTime(tm); // check if it is nighttime OR daytime
     // // test print
-	  // Serial.print("the value of nightTime is now ");
-	  // Serial.println(nightTime);
+	   Serial.print("the value of nightTime is now ");
+	   Serial.println(nightTime);
     
   } else {
     if (RTC.chipPresent()) {
@@ -144,21 +157,20 @@ void loop()
       Serial.println("DS1307 read error!  Please check the circuitry.");
       Serial.println();
     }
-    delay(9000);
+    delay(9000); 
 	  // todo GOTO ALARM	
   }
 
   // read the state of the upperSwitch and place it in the variable upperStateSwitch
-  upperSwitchState = digitalRead(upperSwitch);
+    upperSwitchState = digitalRead(upperSwitch);
   // read the state of the lowerSwitch and place it in the variable lowerStateSwitch
-  lowerSwitchState = digitalRead(lowerSwitch);
-
+    lowerSwitchState = digitalRead(lowerSwitch);
 
   // if it is daytime AND the upperSwitchstate is SWITCH_NOT_ACTIVATED
   if (nightTime == false && upperSwitchState == SWITCH_NOT_ACTIVATED) {
     runMotor1Up();     // call function runMotor1Up()
 	
-	   // // test print
+	  // // test print
     // TEST Serial.print("runMotor1Up running :");
     // TEST Serial.println(upperSwitchState); 
 
@@ -181,22 +193,27 @@ void loop()
     // Serial.print("runMotor1Down stopped :");
     // Serial.println(lowerSwitchState);
   }
+
 }
 
 
 bool checkNightTime(tmElements_t tm){
+
+  // test print
+  Serial.println("Dit is CheckNighTime()");
+  delay(3000);
   
   sunRiseNow = sun_rise[(tm.Month -1)]; // ! (tm.Month-1) because the first element of the array is at index 0 (the array positions count from [0] to [11])
   sunSetNow = sun_set [(tm.Month -1)] + sunSetOffset;  //   // Find the appropriate time in the array sun_set[] and add a safety offset time (e.g. 60 minutes) to avoid "locked-out" chickens.
   clockTimeNow = (tm.Hour-summerTimeOffset) * 60 + tm.Minute; // clockTimeNow in minutes: substract 1 Hour (summerTimeOffset) when the clock runs in summertime 
     
   // //test print
-  // Serial.print("sunRiseNow is ");  
-  // Serial.print(sunRiseNow);
-  // Serial.print("       sunSetNow is ");   
-  // Serial.print(sunSetNow);
-  // Serial.print("       clockTimeNow is ");  
-  // Serial.println(clockTimeNow);
+    Serial.print("sunRiseNow is ");  
+    Serial.print(sunRiseNow);
+    Serial.print("       sunSetNow is ");   
+    Serial.print(sunSetNow);
+    Serial.print("       clockTimeNow is ");  
+    Serial.println(clockTimeNow);
 
   bool nightTime = true; // it is nighttime
   if ((clockTimeNow >= sunRiseNow) && (clockTimeNow <= sunSetNow)){
@@ -213,32 +230,61 @@ bool checkNightTime(tmElements_t tm){
 
 // this function activates runMotor1Up
 void runMotor1Up() {  
+
+  // test print
+    Serial.println("Dit is void runMotor1Up()");
+    delay(3000);
+  
   digitalWrite(IN1,HIGH);
   digitalWrite(IN2, LOW);
-  Serial.println("Run Motor Up"); 
+  
 }
 
 
 // this function activates runMotor1Down
-void runMotor1Down() {  
+void runMotor1Down() { 
+
+
+// test print
+  Serial.println("Dit is void runMotor1Down()");
+  delay(3000);
+
+   
   digitalWrite(IN1,LOW);
   digitalWrite(IN2, HIGH);
-  Serial.println("Run Motor Down"); 
+
+  
 }
 
 
 // this function stops motor1
 void runMotor1Stop() {
+
+
+// test print
+  Serial.println("Dit is void runMotor1Stop()");
+  delay(3000);
+
+  
    digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
- // test print
-  Serial.println("Run Motor Stop");
- // TODO wait 60 seconden before returning to the main loop
- // delay(60000);
- // TEST delay(5000)
-  delay(5000);  
+
 }
 
+void security (int runTimeLimit) {
+  delay(runTimeLimit);
+    // read the state of the upperSwitch and place it in the variable upperStateSwitch
+  upperSwitchState = digitalRead(upperSwitch);
+  // read the state of the lowerSwitch and place it in the variable lowerStateSwitch
+  lowerSwitchState = digitalRead(lowerSwitch);
+  if (upperSwitchState = SWITCH_NOT_ACTIVATED) {
+    Serial.println("Over tijd !");
+  }
+  
+  // test print
+  Serial.println("Dit is void security (int runTimeLimit)");
+  delay(3000);
+}
 
 /*
 // print number as 2-digits in case number is only 1-digit 
